15fe3151444c2aa707a561a29858c1d4ff5d7d2b
wonedd <vinmarv@hotmail.com>
2023-02-17 15:59:52 -0300

    more types

--- main.rs
+++ main.rs
@@ -1,8 +1,18 @@
+
+static mut GLOBAL:u8 = 1; //forma de se declara uma variável global e mutável
+                          //variáveis globais mutáveis são perigosas, pois podem ser alteradas por qualquer função
+                          //obrigatóriamente tipada 
+                          //o rust indica que seja escrita em letras maiúsculas      
+
 fn main(){
 
-  const PI:f32 = 3.14;
-  println!("value = {}, tamanho = {} bytes", PI, std::mem::size_of_val(&PI));
-  
+  unsafe{ //definir que não é segura e que vocÊ sabe o que está fazendo
+    println!("value = {}, tamanho = {} bytes", GLOBAL, std::mem::size_of_val(&GLOBAL));
+  }
+
+  const pi:f32 = 3.14; //obrigatóriamente tipada quando for constante 
+  println!("value = {}, tamanho = {} bytes", pi, std::mem::size_of_val(&pi));
+
   let variable:u8 = 128;
   println!("value = {}, tamanho = {} bytes", variable, std::mem::size_of_val(&variable));
 
